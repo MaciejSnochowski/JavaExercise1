df1fb641f018d34b944c5cc2393c42d898aee847
Maciek <92385355+M4ciejo@users.noreply.github.com>
2022-11-27 11:41:26 +0100

    Poprawiony program

--- src/Read.java
+++ src/Read.java
@@ -1,55 +1,77 @@
-import javax.swing.*;
-import javax.swing.table.TableRowSorter;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+
 public class Read implements Callable<String> {
-    static BufferedReader bufferedReader ;
-  //  Lock lock = new ReentrantLock();
-    private Random random = new Random();
-    //    String anws;
-    static  int counter=0;
+   //BufferReader musi być static ponieważ każdy reader korzysta z niego
+        static BufferedReader bufferedReader ;
+        Lock locker = new ReentrantLock();
+        int counter=0;
+        static ArrayList<Long> listOfIdThread;
 
 
-    public int EnhancedReadLine() throws IOException {
-        bufferedReader.readLine();
-        counter++;
-        return counter;
-    }
+       private int howMany;
 
-    public  Read(FileReader fileReader) throws  IOException,FileNotFoundException{
+    public  Read(FileReader fileReader,int howMany) throws  IOException,FileNotFoundException{
         bufferedReader =new BufferedReader(fileReader);
+        this.howMany=howMany;
+     //   atomicInteger.set(0);
+        listOfIdThread = new ArrayList<Long>();
     }
+//    public int incrementCounter(){
+//
+//         return atomicInteger.getAndIncrement();
+//
+//    }
+    //blokowanie wątku jeśli został wykorzystany 2 razy
+    static public void getList(){
+        System.out.println("init "+ listOfIdThread.get(0));
+        for (long numb:listOfIdThread
+             ) {
+            System.out.println(numb);
 
-
-
+        }
+    }
 
     @Override
-    public String call() throws Exception,IOException {
-        String anwser="";
+    public String call() throws Exception {
+
+        String answer="";
+      // int[] lista=new int[]{};
+      //  int temp=0;
+        //
         try {
 
-                anwser+= bufferedReader.readLine();
-            bufferedReader.mark(1);
 
-
-
-
-
+            answer = bufferedReader.readLine();
                 counter++;
+              // System.out.println("call(). " +answer);
+               // incrementCounter();
+              //  synchronized (locker) {
+
+                  //  counter = atomicInteger.get()
+                Thread.sleep(600);
 
 
-        }finally {
+        }catch (Exception exception){
+            exception.getStackTrace();
+        }
+        finally {
 
-            bufferedReader.close();
-            System.out.println("Wynik czytania => "+anwser + " "+Thread.currentThread().getId());
-            return anwser+"  *  "+random.nextInt(5)+1;
+
+           // listOfIdThread.add(Thread.currentThread().getId());
+            System.out.println("Wynik czytania => "+answer + " Wątek:"+Thread.currentThread().getId());
+            return answer+" :anwser | thread: "+Thread.currentThread().getId();
+
         }
 
     }
